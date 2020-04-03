8a4355c7fddea275201fce6dded2c2613671470a
benjamam <benjamam@miamioh.edu>
2020-04-03 13:57:19 -0400

    will this work?

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const __dirname = 'travel-todo-app';
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/travel-todo-app'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/travel-todo-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
